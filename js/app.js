e35d5c98f9ee63116fd7dc2e551559c78171febe
https://github.com/vanvilas <vanessavilas_boas@outlook.com>
2024-04-20 16:19:35 -0300

    reiniciando o sorteio

--- js/app.js
+++ js/app.js
@@ -38,3 +38,9 @@ function embaralha(lista) {
             [lista[indiceAleatorio], lista[indice - 1]];
     }
 }
+
+function reiniciar() {
+    amigos = [];
+    document.getElementById('lista-amigos').innerHTML = '';
+    document.getElementById('lista-sorteio').innerHTML = '';
+}
