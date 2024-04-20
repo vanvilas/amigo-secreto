7fcc1de3d5c3b9db93a8b83e39167ba8544a1c7b
https://github.com/vanvilas <vanessavilas_boas@outlook.com>
2024-04-20 15:46:36 -0300

    embaralhando o array de amigos

--- js/app.js
+++ js/app.js
@@ -1,12 +1,29 @@
+let amigos = [];
+
 function adicionar() {
     let amigo = document.getElementById('nome-amigo');
     let lista = document.getElementById('lista-amigos');
-    
+    amigos.push(amigo.value);
     if (lista.textContent == '') {
         lista.textContent = amigo.value;
     } else {
         lista.textContent = lista.textContent + ', ' + amigo.value;
     }
     amigo.value = '';
+}
 
+function sortear() {
+    embaralha(amigos);
+}
+
+function embaralha(lista) {
+
+    for (let indice = lista.length; indice; indice--) {
+
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+
+        // atribuição via destructuring
+        [lista[indice - 1], lista[indiceAleatorio]] = 
+            [lista[indiceAleatorio], lista[indice - 1]];
+    }
 }
