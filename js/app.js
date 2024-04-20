3c14c65a62ea01112eaf0554bcd26868657b8ded
https://github.com/vanvilas <vanessavilas_boas@outlook.com>
2024-04-20 16:08:53 -0300

    realizando o sorteio

--- js/app.js
+++ js/app.js
@@ -14,6 +14,17 @@ function adicionar() {
 
 function sortear() {
     embaralha(amigos);
+    let sorteio = document.getElementById('lista-sorteio');
+
+    for (let i = 0; i < amigos.length; i++) {
+
+        if (i == amigos.length - 1) {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + '--> ' + amigos[0] + '<br>';
+        } else {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + '--> ' + amigos[i + 1] + '<br>';
+        }
+        
+    }
 }
 
 function embaralha(lista) {
